dd0706828021ebbd12d5df0fe1e344fee31cda9b
Your Name <you@example.com>
2023-11-13 13:00:27 +0600

    javascript to do list

---
+++ script.js
@@ -0,0 +1,70 @@
+const selectItem = document.querySelector('.body ul');
+const selectForm = document.querySelector('.todo');
+
+selectItem.addEventListener('click', (e) => {
+  if(e.target.tagName==='LI') {
+    if(e.target.classList.contains('done')) {
+      e.target.classList.remove('done');
+    } else {
+      e.target.classList.add('done');
+    }
+  }
+
+  if(e.target.classList.contains('remove')) {
+    e.target.parentNode.remove()
+  }
+})
+
+const removeAll = document.querySelector('.removeAll')
+
+
+
+selectForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = e.target.task.value;
+  if(validateInput(input, e.target.task)) {
+    selectItem.insertAdjacentElement('afterbegin', newItem(e.target.task.value));
+    e.target.task.value = '';
+  }
+});
+
+const validateInput = (input, element) => {
+  if(input) {
+    element.parentNode.classList.remove('error');
+    return true;
+  } else {
+    element.parentNode.classList.add('error');
+    return false;
+  }
+}
+
+const newItem = (content) => {
+  const createAItem = document.createElement('li');
+  createAItem.textContent = content;
+  createAItem.insertAdjacentElement('afterbegin', removeButton());
+  return createAItem;
+}
+removeAll.addEventListener('click', function() {
+  if(selectItem) {
+    selectItem.remove()
+  } else{
+    selectItem.classList.add()
+  }
+})
+const removeButton = () => {
+  const createRemoveBtn = document.createElement('span');
+  createRemoveBtn.classList.add('remove');
+  createRemoveBtn.textContent = 'X';
+  return createRemoveBtn;
+}
+
+
+
+
+
+
+const numbers = [10, 20, 30, 40, 50]
+
+const result = numbers.push([60, 70])
+
+console.log(result);
